4868e15d3a02f41b00bb1beb12eb9a90f82730d8
mrprokoala <zunigaruizb@gmail.com>
2020-07-12 04:53:02 +0800

    setup: stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2020: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
