033981ad0e85068cf54e4e9d4643294baaa050a6
mrprokoala <zunigaruizb@gmail.com>
2020-07-19 06:49:32 +0800

    fix: header grids

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -7,24 +7,28 @@ import { rem } from 'polished';
 const Header = ({ history }) => {
   return (
     <Grid container style={{ marginBottom: rem('12px') }}>
-      <Grid container xs={4}>
+      <Grid item xs={4}>
         <Typography variant='h5'>RemoteNC Cloud Mill</Typography>
       </Grid>
-      <Grid container xs={4} justify='center'>
-        <Typography variant='h5' style={{ fontWeight: 500 }}>
-          Job Name / Company Name
-        </Typography>
+      <Grid item xs={4}>
+        <Grid container justify='center'>
+          <Typography variant='h5' style={{ fontWeight: 500 }}>
+            Job Name / Company Name
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid container xs={4} justify='flex-end'>
-        <Button
-          color='primary'
-          variant='contained'
-          onClick={() => {
-            history.push('/');
-          }}
-        >
-          DISCONNECT
-        </Button>
+      <Grid item xs={4}>
+        <Grid container justify='flex-end'>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={() => {
+              history.push('/');
+            }}
+          >
+            DISCONNECT
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
