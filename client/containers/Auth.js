5faafb6e23160a09aab9a6e942e157aaab334590
balta-z-r <zunigaruizb@gmail.com>
2020-08-02 06:32:10 +0800

    feat: log in page

--- client/containers/Auth.js
+++ client/containers/Auth.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { Typography, Container } from '@material-ui/core';
-import { MachineBox } from '../components';
-
-const TEMP_DATA = [
-  { number: 1, clientName: 'Henry Wu', jobName: 'Aluminum Clevis Mount' },
-  { number: 2, clientName: 'Baltazar Ruiz', jobName: '6061 Bellcrank Shaft' },
-  { number: 3, clientName: 'Alex Mei', jobName: '57839-78' }
-];
+import {
+  Typography,
+  TextField,
+  Card,
+  CardContent,
+  Grid,
+  Button
+} from '@material-ui/core';
 
 const Auth = () => {
   return (
-    <Container maxWidth='md'>
-      <Typography variant='h5' gutterBottom>
-        Please Select a Machine to Connect:
-      </Typography>
-      {TEMP_DATA.map(({ number, clientName, jobName }) => (
-        <MachineBox
-          number={number}
-          clientName={clientName}
-          jobName={jobName}
-          key={number}
-        />
-      ))}
-    </Container>
+    <Grid
+      container
+      justify='center'
+      alignItems='center'
+      style={{ height: '90vh' }}
+    >
+      <Grid item xs={2}>
+        <Card>
+          <CardContent>
+            <Typography variant='h5' gutterBottom>
+              Log in
+            </Typography>
+            <TextField required id='standard-required' label='Email' />
+            <TextField
+              id='standard-password-input'
+              label='Password'
+              type='password'
+              autoComplete='current-password'
+            />
+            <div style={{ marginTop: 12 }}>
+              <Button variant='contained' color='primary'>
+                Submit
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
