23387c1d5e038bd9cace2e7f2b49c5b4a377aa00
balta-z-r <zunigaruizb@gmail.com>
2020-08-16 15:43:02 +0800

    feat: press enter in password input to submit

--- client/containers/Auth.js
+++ client/containers/Auth.js
@@ -14,6 +14,16 @@ const Auth = () => {
   const [password, setPassword] = useState('');
   const { login } = useAuth();
 
+  const submit = () => {
+    login(email, password);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      submit();
+    }
+  };
+
   return (
     <Grid
       container
@@ -39,17 +49,12 @@ const Auth = () => {
               label='Password'
               type='password'
               autoComplete='current-password'
+              onKeyDown={handleKeyDown}
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
             <div style={{ marginTop: 12 }}>
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={() => {
-                  login(email, password);
-                }}
-              >
+              <Button variant='contained' color='primary' onClick={submit}>
                 Submit
               </Button>
             </div>
