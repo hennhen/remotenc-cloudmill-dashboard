a0196c147d0b02d071d4fa2a43d5650f39440976
Henry Wu <henrywuu1122@gmail.com>
2020-09-26 21:35:28 +0800

    aligned naming and data structure convensions

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,200 +1,224 @@
-import React, { useContext, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import {
-  Container,
-  Grid,
-  Divider,
-  Button,
-  Typography,
-  Switch
-} from '@material-ui/core';
+   Container,
+   Grid,
+   Divider,
+   Button,
+   Typography,
+   Switch,
+} from "@material-ui/core";
 import {
-  SideBar,
-  Video,
-  DataBox,
-  GCodeBox,
-  SettingBox,
-  SliderBox
-} from '../components';
-import { useWebRTC } from '../hooks';
-import { withStyles } from '@material-ui/core/styles';
-import { green, red, yellow } from '@material-ui/core/colors';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { JobContext } from '../context';
+   SideBar,
+   Video,
+   DataBox,
+   GCodeBox,
+   SettingBox,
+   SliderBox,
+} from "../components";
+import { useWebRTC } from "../hooks";
+import { withStyles } from "@material-ui/core/styles";
+import { green, red, yellow } from "@material-ui/core/colors";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { JobContext } from "../context";
 
 const topHeight = 420;
 const bottomHeight = 270;
 
 const GreenButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(green[700]),
-    backgroundColor: green[700],
-    '&:hover': {
-      backgroundColor: green[900]
-    }
-  }
+   root: {
+      color: theme.palette.getContrastText(green[700]),
+      backgroundColor: green[700],
+      "&:hover": {
+         backgroundColor: green[900],
+      },
+   },
 }))(Button);
 
 const RedButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(red[700]),
-    backgroundColor: red[700],
-    '&:hover': {
-      backgroundColor: red[900]
-    }
-  }
+   root: {
+      color: theme.palette.getContrastText(red[700]),
+      backgroundColor: red[700],
+      "&:hover": {
+         backgroundColor: red[900],
+      },
+   },
 }))(Button);
 
 const YellowButton = withStyles((theme) => ({
-  root: {
-    color: theme.palette.getContrastText(yellow[700]),
-    backgroundColor: yellow[700],
-    '&:hover': {
-      backgroundColor: yellow[800]
-    }
-  }
+   root: {
+      color: theme.palette.getContrastText(yellow[700]),
+      backgroundColor: yellow[700],
+      "&:hover": {
+         backgroundColor: yellow[800],
+      },
+   },
 }))(Button);
 
 const Dashboard = () => {
-  const { job, setJob } = useContext(JobContext);
-  const history = useHistory();
+   const { job, setJob } = useContext(JobContext);
+   const history = useHistory();
 
-  const { videoOne, videoTwo, data, connected } = useWebRTC(
-    (job && job.machine.ip_address) || 'none'
-  );
+   const { videoOne, videoTwo, data, connected } = useWebRTC(
+      (job && job.machine.ip_address) || "none"
+   );
 
-  const dashboardNode = job ? (
-    <Container>
-      <Grid container spacing={3}>
-        <Grid item xs={2}>
-          <div style={{ padding: '10px 0' }}>
-            <ArrowBackIcon
-              fontSize='large'
-              color='primary'
-              style={{ cursor: 'pointer' }}
-              onClick={() => {
-                setJob(undefined);
-                history.push('/jobs');
-              }}
-            />
-          </div>
-          <SideBar name={job.name} />
-        </Grid>
-        <Divider orientation='vertical' flexItem />
-        <Grid item xs={9}>
-          <Grid container spacing={3}>
-            <Grid item xs={4}>
-              <Grid
-                container
-                direction='column'
-                justify='space-between'
-                alignItems='flex-start'
-                style={{ height: topHeight }}
-              >
-                <Grid item>{connected && <Video ref={videoOne} />}</Grid>
-                <Grid item>{connected && <Video ref={videoTwo} />}</Grid>
-              </Grid>
+   const dashboardNode = job ? (
+      <Container>
+         <Grid container spacing={3}>
+            <Grid item xs={2}>
+               <div style={{ padding: "10px 0" }}>
+                  <ArrowBackIcon
+                     fontSize="large"
+                     color="primary"
+                     style={{ cursor: "pointer" }}
+                     onClick={() => {
+                        setJob(undefined);
+                        history.push("/jobs");
+                     }}
+                  />
+               </div>
+               <SideBar name={job.name} />
             </Grid>
-            <Grid item xs={4}>
-              <Grid
-                container
-                direction='column'
-                justify='space-between'
-                alignItems='stretch'
-                style={{ height: topHeight }}
-              >
-                <Grid item>
-                  <DataBox label='X' value={data.x}></DataBox>
-                </Grid>
-                <Grid item>
-                  <DataBox label='Y' value={data.y}></DataBox>
-                </Grid>
-                <Grid item>
-                  <DataBox label='Z' value={data.z}></DataBox>
-                </Grid>
-                <Grid item>
-                  <DataBox label='A' value={data.a}></DataBox>
-                </Grid>
-                <Grid item>
-                  <DataBox label='C' value={data.c}></DataBox>
-                </Grid>
-              </Grid>
+            <Divider orientation="vertical" flexItem />
+            <Grid item xs={9}>
+               <Grid container spacing={3}>
+                  <Grid item xs={4}>
+                     <Grid
+                        container
+                        direction="column"
+                        justify="space-between"
+                        alignItems="flex-start"
+                        style={{ height: topHeight }}
+                     >
+                        <Grid item>
+                           {connected && <Video ref={videoOne} />}
+                        </Grid>
+                        <Grid item>
+                           {connected && <Video ref={videoTwo} />}
+                        </Grid>
+                     </Grid>
+                  </Grid>
+                  <Grid item xs={4}>
+                     <Grid
+                        container
+                        direction="column"
+                        justify="space-between"
+                        alignItems="stretch"
+                        style={{ height: topHeight }}
+                     >
+                        <Grid item>
+                           <DataBox label="X" value={data.coors.x}></DataBox>
+                        </Grid>
+                        <Grid item>
+                           <DataBox label="Y" value={data.coors.y}></DataBox>
+                        </Grid>
+                        <Grid item>
+                           <DataBox label="Z" value={data.coors.z}></DataBox>
+                        </Grid>
+                        <Grid item>
+                           <DataBox label="A" value={data.coors.a}></DataBox>
+                        </Grid>
+                        <Grid item>
+                           <DataBox label="C" value={data.coors.c}></DataBox>
+                        </Grid>
+                     </Grid>
+                  </Grid>
+                  <Grid item xs={4}>
+                     <Grid
+                        container
+                        direction="column"
+                        justify="space-between"
+                        alignItems="stretch"
+                        style={{ height: topHeight }}
+                     >
+                        <GCodeBox
+                           gCode={job.gcode_array}
+                           currentIdx={data.line_num}
+                        />
+                     </Grid>
+                  </Grid>
+               </Grid>
+               <Grid container spacing={3} style={{ height: bottomHeight }}>
+                  <Grid item xs={3}>
+                     <Grid container direction="column" spacing={2}>
+                        <Grid item>
+                           <GreenButton
+                              color="primary"
+                              variant="contained"
+                              fullWidth
+                           >
+                              Start
+                           </GreenButton>
+                        </Grid>
+                        <Grid item>
+                           <RedButton
+                              color="primary"
+                              variant="contained"
+                              fullWidth
+                           >
+                              Stop
+                           </RedButton>
+                        </Grid>
+                        <Grid item>
+                           <YellowButton
+                              color="primary"
+                              variant="contained"
+                              fullWidth
+                           >
+                              Pause
+                           </YellowButton>
+                        </Grid>
+                     </Grid>
+                  </Grid>
+                  <Grid item xs={3}>
+                     <Grid
+                        container
+                        direction="column"
+                        justify="space-between"
+                        alignItems="stretch"
+                     >
+                        <Grid item>
+                           <SettingBox name="Tool">
+                              <Grid container>
+                                 <Grid item xs={6}>
+                                    <Typography variant="h6">
+                                       Tool Dia:{" "}
+                                    </Typography>
+                                 </Grid>
+                                 <Grid item xs={6}>
+                                    <Typography
+                                       variant="h6"
+                                       style={{ fontWeight: 400 }}
+                                    >
+                                       {'1/2"'}
+                                    </Typography>
+                                 </Grid>
+                              </Grid>
+                           </SettingBox>
+                        </Grid>
+                        <Grid item>
+                           <SettingBox name="Coolant">
+                              <Grid container justify="flex-end">
+                                 <Switch color="primary" />
+                              </Grid>
+                           </SettingBox>
+                        </Grid>
+                     </Grid>
+                  </Grid>
+                  <Grid item xs={3}>
+                     <SliderBox name="Spindle" actual={1495} max={3000} />
+                  </Grid>
+                  <Grid item xs={3}>
+                     <SliderBox name="Feed Rate" actual={48} max={100} />
+                  </Grid>
+               </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <Grid
-                container
-                direction='column'
-                justify='space-between'
-                alignItems='stretch'
-                style={{ height: topHeight }}
-              >
-                <GCodeBox gCode={job.gcode_array} currentIdx={data.line_num} />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3} style={{ height: bottomHeight }}>
-            <Grid item xs={3}>
-              <Grid container direction='column' spacing={2}>
-                <Grid item>
-                  <GreenButton color='primary' variant='contained' fullWidth>
-                    Start
-                  </GreenButton>
-                </Grid>
-                <Grid item>
-                  <RedButton color='primary' variant='contained' fullWidth>
-                    Stop
-                  </RedButton>
-                </Grid>
-                <Grid item>
-                  <YellowButton color='primary' variant='contained' fullWidth>
-                    Pause
-                  </YellowButton>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={3}>
-              <Grid
-                container
-                direction='column'
-                justify='space-between'
-                alignItems='stretch'
-              >
-                <Grid item>
-                  <SettingBox name='Tool'>
-                    <Grid container>
-                      <Grid item xs={6}>
-                        <Typography variant='h6'>Tool Dia: </Typography>
-                      </Grid>
-                      <Grid item xs={6}>
-                        <Typography variant='h6' style={{ fontWeight: 400 }}>
-                          {'1/2"'}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </SettingBox>
-                </Grid>
-                <Grid item>
-                  <SettingBox name='Coolant'>
-                    <Grid container justify='flex-end'>
-                      <Switch color='primary' />
-                    </Grid>
-                  </SettingBox>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={3}>
-              <SliderBox name='Spindle' actual={1495} max={3000} />
-            </Grid>
-            <Grid item xs={3}>
-              <SliderBox name='Feed Rate' actual={48} max={100} />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
-  ) : null;
+         </Grid>
+      </Container>
+   ) : null;
 
-  return dashboardNode;
+   return dashboardNode;
 };
 
 export { Dashboard };
--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -1,103 +1,106 @@
-import { AlertContext } from '../context';
+import { AlertContext } from "../context";
 
-import { useEffect, useState, useRef, useContext } from 'react';
-import Peer from 'simple-peer';
-import io from 'socket.io-client';
-import { useHistory } from 'react-router-dom';
+import { useEffect, useState, useRef, useContext } from "react";
+import Peer from "simple-peer";
+import io from "socket.io-client";
+import { useHistory } from "react-router-dom";
 
 const useWebRTC = (ip: string) => {
-  const [data, setData] = useState({ x: 0, y: 0, z: 0, a: 0, c: 0 });
+   const [data, setData] = useState({
+      coors: { x: 0, y: 0, z: 0, a: 0, c: 0 },
+      line_num: 0,
+   });
 
-  const videoOne = useRef<HTMLVideoElement>(null);
-  const videoTwo = useRef<HTMLVideoElement>(null);
-  const socket = useRef<any>(null);
-  const peer = useRef<Peer.Instance>();
-  const connected = useRef<boolean>();
-  const disconnect = useRef<boolean>();
+   const videoOne = useRef<HTMLVideoElement>(null);
+   const videoTwo = useRef<HTMLVideoElement>(null);
+   const socket = useRef<any>(null);
+   const peer = useRef<Peer.Instance>();
+   const connected = useRef<boolean>();
+   const disconnect = useRef<boolean>();
 
-  const { setAlert } = useContext(AlertContext);
+   const { setAlert } = useContext(AlertContext);
 
-  const history = useHistory();
+   const history = useHistory();
 
-  useEffect(() => {
-    const connectSocket = async () => {
-      try {
-        socket.current = await io(ip);
-        socket.current.on('connect', () => {
-          console.log('socket connected');
-          peer.current = buildPeer();
-        });
-        socket.current.on('rtc', (signal: string) => {
-          if (peer.current) peer.current.signal(signal);
-        });
-      } catch (err) {
-        setAlert({
-          type: 'error',
-          message: 'Unable to connect to server, please come back later.'
-        });
-        history.push('/jobs');
-      }
-    };
+   useEffect(() => {
+      const connectSocket = async () => {
+         try {
+            socket.current = await io(ip);
+            socket.current.on("connect", () => {
+               console.log("socket connected");
+               peer.current = buildPeer();
+            });
+            socket.current.on("rtc", (signal: string) => {
+               if (peer.current) peer.current.signal(signal);
+            });
+         } catch (err) {
+            setAlert({
+               type: "error",
+               message: "Unable to connect to server, please come back later.",
+            });
+            history.push("/jobs");
+         }
+      };
 
-    connectSocket();
+      connectSocket();
 
-    return () => {
-      disconnect.current = true;
-      socket.current.close();
-      if (peer.current) peer.current.destroy();
-    };
-  }, []);
+      return () => {
+         disconnect.current = true;
+         socket.current.close();
+         if (peer.current) peer.current.destroy();
+      };
+   }, []);
 
-  const buildPeer = () => {
-    const newPeer = new Peer({
-      initiator: true,
-      config: {
-        iceServers: [
-          {
-            urls: 'stun:stun.remotenc.com:5349'
-          },
-          {
-            urls: 'turn:turn.remotenc.com:5349',
-            username: 'user',
-            credential: '12345678'
-          }
-        ]
-      }
-    });
-
-    newPeer.on('signal', (signal: string) => {
-      socket.current.emit('rtc', signal);
-    });
-
-    newPeer.on('stream', (stream) => {
-      if (!videoOne.current || !videoTwo.current) return;
-      connected.current = true;
-      if (!videoOne.current.srcObject) videoOne.current.srcObject = stream;
-      else videoTwo.current.srcObject = stream;
-    });
-
-    newPeer.on('data', (dataString) => {
-      const data = JSON.parse(dataString);
-      setData(data);
-    });
-
-    newPeer.on('error', (err: any) => {
-      console.error(err.code);
-    });
-
-    newPeer.on('close', () => {
-      if (disconnect.current) return;
-      socket.current.off('rtc');
-      peer.current = buildPeer();
-      socket.current.on('rtc', (signal: string) => {
-        if (peer.current) peer.current.signal(signal);
+   const buildPeer = () => {
+      const newPeer = new Peer({
+         initiator: true,
+         config: {
+            iceServers: [
+               {
+                  urls: "stun:stun.remotenc.com:5349",
+               },
+               {
+                  urls: "turn:turn.remotenc.com:5349",
+                  username: "user",
+                  credential: "12345678",
+               },
+            ],
+         },
       });
-    });
 
-    return newPeer;
-  };
+      newPeer.on("signal", (signal: string) => {
+         socket.current.emit("rtc", signal);
+      });
 
-  return { videoOne, videoTwo, data, connected };
+      newPeer.on("stream", (stream) => {
+         if (!videoOne.current || !videoTwo.current) return;
+         connected.current = true;
+         if (!videoOne.current.srcObject) videoOne.current.srcObject = stream;
+         else videoTwo.current.srcObject = stream;
+      });
+
+      newPeer.on("data", (dataString) => {
+         const data = JSON.parse(dataString);
+         setData(data);
+      });
+
+      newPeer.on("error", (err: any) => {
+         console.error(err.code);
+      });
+
+      newPeer.on("close", () => {
+         if (disconnect.current) return;
+         socket.current.off("rtc");
+         peer.current = buildPeer();
+         socket.current.on("rtc", (signal: string) => {
+            if (peer.current) peer.current.signal(signal);
+         });
+      });
+
+      return newPeer;
+   };
+
+   return { videoOne, videoTwo, data, connected };
 };
 
 export { useWebRTC };
